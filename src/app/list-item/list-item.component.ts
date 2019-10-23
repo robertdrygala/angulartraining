196654dbe513027ebae9fd324e539bb1c0442ab5
Robert <robert.drygala@gmail.com>
2019-10-23 12:22:10 +0200

    more settings

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,30 +1,25 @@
 import { Component, OnInit, Input, OnChanges, EventEmitter, Output } from '@angular/core';
 import { CourseItem } from '../model/course-item';
 
-import {SimpleChanges } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.less']
+  styleUrls: ['./list-item.component.less'],
 })
 export class ListItemComponent implements OnInit, OnChanges {
-
   @Output() removeItem = new EventEmitter<CourseItem>();
 
   @Input() public todoItem: CourseItem = new CourseItem();
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-  }
+  ngOnChanges(changes: SimpleChanges): void {}
 
   public remove(): void {
     this.removeItem.emit(this.todoItem);
   }
-
 }
