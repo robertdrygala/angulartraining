3e1ce1b7aad64eecc7f7fef9d4772113eda7ea8e
Robert <robert.drygala@gmail.com>
2019-10-25 12:41:24 +0200

    build page

--- src/app/course-details/course-details.component.ts
+++ src/app/course-details/course-details.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CourseItem } from '../model/course-item';
 
+const TITLE = 'Video Course 1. Name tag';
+// tslint:disable-next-line: max-line-length
+const DESCRIPTION = `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.`;
+
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
@@ -15,23 +19,23 @@ export class CourseDetailsComponent implements OnInit {
     this.todoItems = [
       {
         id: '1',
-        title: 'First',
+        title: TITLE,
         creationDate: new Date(),
-        description: 'Descr',
+        description: DESCRIPTION,
         duration: 1,
       },
       {
         id: '2',
-        title: 'Second',
+        title: TITLE,
         creationDate: new Date(),
-        description: 'Descr',
+        description: DESCRIPTION,
         duration: 1,
       },
       {
         id: '3',
-        title: 'Third',
+        title: TITLE,
         creationDate: new Date(),
-        description: 'Descr',
+        description: DESCRIPTION,
         duration: 1,
       },
     ];
