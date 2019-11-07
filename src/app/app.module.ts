3ffbf2cb505d3b865db7c7329cd1bc6e7c5eec9f
Robert <robert.drygala@gmail.com>
2019-11-07 10:19:48 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @NgModule({
   declarations: [AppComponent, FooterComponent, HeaderComponent, CourseDetailsComponent, ListItemComponent],
   imports: [BrowserModule, AppRoutingModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
