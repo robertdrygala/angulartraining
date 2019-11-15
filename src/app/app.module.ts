57a13f3121880f73198b694e0b1a22838ad94963
Robert <robert.drygala@gmail.com>
2019-11-15 11:42:57 +0100

    PR remarks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,10 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
 import { ListItemComponent } from './list-item/list-item.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent, HeaderComponent, CourseDetailsComponent, ListItemComponent],
   imports: [BrowserModule, AppRoutingModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
