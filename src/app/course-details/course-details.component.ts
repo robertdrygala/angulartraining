7ee24fae050572dfee090ec74995a91ba20e0174
Robert <robert.drygala@gmail.com>
2019-11-21 09:58:38 +0100

    Add course service

--- src/app/course-details/course-details.component.ts
+++ src/app/course-details/course-details.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CourseItem } from '../model/course-item';
-
-export const definition = {
-  TITLE: 'Video Course 1. Name tag',
-  DESCRIPTION:
-    // tslint:disable-next-line: max-line-length
-    'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or colleges classes. Theyre published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.',
-};
+import { CourseServiceService } from '../services/course-service.service';
 
 @Component({
   selector: 'app-course-details',
@@ -14,76 +8,11 @@ export const definition = {
   styleUrls: ['./course-details.component.less'],
 })
 export class CourseDetailsComponent implements OnInit {
-  todoItems: CourseItem[] = [];
   titleFilter = 'Non';
 
-  constructor() {}
+  constructor(public courseService: CourseServiceService) {}
 
-  ngOnInit() {
-    let oldCourse = new Date();
-    let year = oldCourse.getFullYear();
-    oldCourse.setFullYear(year - 1);
-
-    let futureCourse = new Date();
-    year = futureCourse.getFullYear();
-    futureCourse.setFullYear(year + 1);
-
-    let quiteANewOne = new Date();
-    let hour = quiteANewOne.getHours();
-    quiteANewOne.setHours(hour - 1);
-
-    this.todoItems = [
-      {
-        id: '1',
-        title: definition.TITLE,
-        creationDate: futureCourse,
-        description: definition.DESCRIPTION,
-        duration: 134,
-        topRated: true,
-      },
-      {
-        id: '2',
-        title: definition.TITLE,
-        creationDate: quiteANewOne,
-        description: definition.DESCRIPTION,
-        duration: 15,
-        topRated: false,
-      },
-      {
-        id: '3',
-        title: definition.TITLE,
-        creationDate: oldCourse,
-        description: definition.DESCRIPTION,
-        duration: 340,
-        topRated: true,
-      },
-      {
-        id: '4',
-        title: definition.TITLE,
-        creationDate: quiteANewOne,
-        description: definition.DESCRIPTION,
-        duration: 133,
-        topRated: false,
-      },
-      {
-        id: '5',
-        title: 'Non video',
-        creationDate: quiteANewOne,
-        description: definition.DESCRIPTION,
-        duration: 133,
-        topRated: false,
-      },
-    ];
-  }
-
-  public removeItem(item: CourseItem) {
-    console.log('Item ' + item.title + ' has been choosen to be removed....');
-    const index = this.todoItems.indexOf(item, 0);
-    if (index > -1) {
-      console.log('Item ' + item.title + ' has been removed....');
-      this.todoItems.splice(index, 1);
-    }
-  }
+  ngOnInit() {}
 
   public calculateClass(item: CourseItem) {
     if (item.topRated) {
---
+++ src/app/services/course-service.service.ts
@@ -0,0 +1,101 @@
+import { Injectable, Inject } from '@angular/core';
+import { APP_STORAGE } from '../core/core.module';
+import { CourseItem } from '../model/course-item';
+import { Course } from '../model/course';
+
+export const definition = {
+  TITLE: 'Video Course 1. Name tag',
+  DESCRIPTION:
+    // tslint:disable-next-line: max-line-length
+    'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or colleges classes. Theyre published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.',
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CourseServiceService {
+  // constructor(@Inject(APP_STORAGE) private storage: Storage) {}
+  constructor() {
+    this.init();
+  }
+
+  todoItems: CourseItem[] = [];
+
+  init() {
+    let oldCourse = new Date();
+    let year = oldCourse.getFullYear();
+    oldCourse.setFullYear(year - 1);
+
+    let futureCourse = new Date();
+    year = futureCourse.getFullYear();
+    futureCourse.setFullYear(year + 1);
+
+    let quiteANewOne = new Date();
+    let hour = quiteANewOne.getHours();
+    quiteANewOne.setHours(hour - 1);
+
+    this.todoItems = [
+      {
+        id: '1',
+        title: definition.TITLE,
+        creationDate: futureCourse,
+        description: definition.DESCRIPTION,
+        duration: 134,
+        topRated: true,
+      },
+      {
+        id: '2',
+        title: definition.TITLE,
+        creationDate: quiteANewOne,
+        description: definition.DESCRIPTION,
+        duration: 15,
+        topRated: false,
+      },
+      {
+        id: '3',
+        title: definition.TITLE,
+        creationDate: oldCourse,
+        description: definition.DESCRIPTION,
+        duration: 340,
+        topRated: true,
+      },
+      {
+        id: '4',
+        title: definition.TITLE,
+        creationDate: quiteANewOne,
+        description: definition.DESCRIPTION,
+        duration: 133,
+        topRated: false,
+      },
+      {
+        id: '5',
+        title: 'Non video',
+        creationDate: quiteANewOne,
+        description: definition.DESCRIPTION,
+        duration: 133,
+        topRated: false,
+      },
+    ];
+  }
+
+  getList() {
+    return this.todoItems;
+  }
+
+  createCourse(course: Course) {
+    // this.storage.setItem('course', course.title);
+  }
+
+  getItemById() {}
+
+  updateItem() {}
+
+  public removeItem(item: CourseItem) {
+    console.log('Item ' + item.title + ' has been choosen to be removed....');
+    const index = this.todoItems.indexOf(item, 0);
+    if (index > -1) {
+      console.log('Item ' + item.title + ' has been removed....');
+      this.todoItems.splice(index, 1);
+    }
+  }
+}
